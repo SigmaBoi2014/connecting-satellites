4559808d456082d6639ba0328f384e2fd111e93b
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-04-09 07:59:54 -0700

    Add files via upload

--- connecting satellites.py
+++ connecting satellites.py
@@ -1,12 +1,31 @@
 import random
 import pgzrun
-
 WIDTH=600
 HEIGHT=450
-satellite=Actor("satellite")
+sat=[]
+next=0
+lines=[]
+for i in range(10):
+    satellite=Actor("satellite")
+    satellite.x = random.randint(30,560)
+    satellite.y = random.randint(30,420)
+    sat.append(satellite)
 def draw():
     screen.blit("background",(0,0))
-    satellite.draw()
+    num=1
+    for i in sat:
+        i.draw()
+        screen.draw.text(str(num),(i.x,i.y+15),color = "red")
+        num=num+1
+    for line in lines:
+        screen.draw.line(line[0],line[1],"red")
+def on_mouse_down(pos):
+    global next,sat,lines
+    if sat[next].collidepoint(pos):
+        if next>0:
+            lines.append((sat[next-1].pos,sat[next].pos))
+            print(lines)
+        next=next+1
 
 
 
