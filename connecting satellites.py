8bdffe9a7398555b5e81ff6fce24420bfbef80e8
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-04-30 07:06:59 -0700

    Add files via upload

--- connecting satellites.py
+++ connecting satellites.py
@@ -3,6 +3,8 @@ import pgzrun
 from time import time
 WIDTH=600
 HEIGHT=450
+game_over=False
+
 start = time()
 sat=[]
 next=0
@@ -29,8 +31,17 @@ def draw():
         screen.draw.text(str(round(total,2)),(10,10),color="yellow")
     else:
         screen.draw.text(str(round(total,2)),(10,10),color="yellow")
+    if game_over:
+        screen.fill("black")
+        screen.draw.text("Time's up! You lost",midtop=(WIDTH/2,10),fontsize=40,color="red")
+    if(next==10):
+        screen.fill("black")
+        screen.draw.text("Good Job! You Won!",midtop=(WIDTH/2,10),fontsize=40,color="green")
 def update():
     pass
+def time_up():
+    global game_over,you_won
+    game_over=True
 def on_mouse_down(pos):
     global next,sat,lines
     if sat[next].collidepoint(pos):
@@ -41,6 +52,7 @@ def on_mouse_down(pos):
     else:
         lines=[]
         next=0
+clock.schedule(time_up,30.00)
 
 
 pgzrun.go()
